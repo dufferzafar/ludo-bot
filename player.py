4c5af444f1d9233a7434a077dfee2b04ca582639
Shadab Zafar <dufferzafar0@gmail.com>
2017-09-22 22:51:54 +0530

    Add Coin class

---
+++ player.py
@@ -0,0 +1,39 @@
+"""
+This module deals with the logical representation of a Player and Coins.
+"""
+
+
+class Coin:
+
+    """Represent a player's coin piece."""
+
+    def __init__(self, color, idx):
+        # The (color, num) pair can uniquely identify a coin
+        self.color = color
+        self.num = idx
+
+        # Position of the coin on the board
+        self._rel_pos = 0
+        self.abs_pos = 0
+
+    def __repr__(self):
+        return self.color[0] + str(self.num)
+
+    @property
+    def rel_pos(self):
+        """
+        Position of this coin relative to the a player.
+
+        Is in range(0, 58)
+
+        Some functions are simpler when using the relative position.
+        While some need an absolute position which is calculated later.
+        """
+        return self._rel_pos
+
+    @rel_pos.setter
+    def rel_pos(self, square):
+        self._rel_pos = square
+
+        # TODO: Update absolute position
+        self.abs_pos = square
