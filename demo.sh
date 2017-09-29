bd1fd5511f936de0a8ac3a2d2156d68536522884
Shadab Zafar <dufferzafar0@gmail.com>
2017-09-29 09:51:59 +0530

    Update demo.sh to run only 1 client with GUI

--- demo.sh
+++ demo.sh
@@ -1,9 +1,16 @@
 echo $1 # foldername code or demo1 or demo2
+
 xterm -hold -title "Server" -e "$1/server/server 10000" &
 sleep 1
+
 xterm -hold -title "Player1" -e "$1/client/client 0.0.0.0 10000 main.py" &
 sleep 1
-xterm -hold -title "Player2" -e "$1/client/client 0.0.0.0 10000 main.py" &
+
+xterm -hold -title "Player2" -e "$1/client/client 0.0.0.0 10000 --noBoard main.py" &
+
 echo "Press any key to kill all xterms"
 read
+
+# pkill -SIGINT -f "server/server"
+pkill -SIGINT -f "client/client 0.0.0.0"
 killall xterm
