eec85af3356930897640d9667710cdcb24228986
Harish <hthuwal@gmail.com>
2017-09-29 11:28:39 +0100

    Log xterms outputs to file

--- demo.sh
+++ demo.sh
@@ -1,12 +1,12 @@
 echo $1 # foldername code or demo1 or demo2
 
-xterm -hold -title "Server" -e "$1/server/server 10000" &
+xterm -hold -title "Server" -l -lf "server_log.txt" -e "$1/server/server 10000" &
 sleep 1
 
-xterm -hold -title "Player1" -e "$1/client/client 0.0.0.0 10000 main.py" &
+xterm -hold -title "Player0" -l -lf "player0_log.txt" -e "$1/client/client 0.0.0.0 10000 main.py" &
 sleep 1
 
-xterm -hold -title "Player2" -e "$1/client/client 0.0.0.0 10000 --noBoard main.py" &
+xterm -hold -title "Player1" -l -lf "player1_log.txt" -e "$1/client/client 0.0.0.0 10000 --noBoard main.py" &
 
 echo "Press any key to kill all xterms"
 read
